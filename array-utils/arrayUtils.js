48966d9e8429f47d62a137f52b001aadb67b4b1f
anshuman2 <60535794+anshuman2@users.noreply.github.com>
2020-12-19 05:42:34 -0800

    Update arrayUtils.js

--- array-utils/arrayUtils.js
+++ array-utils/arrayUtils.js
@@ -1,7 +1,34 @@
+Array.prototype.myMap = function(callback) {
+    let newArray = []
+    for (let i = 0; i < this.length; i++) {
+      let newMappedItem = callback(this[i], i, this)
+      newArray.push(newMappedItem)
+    }
+    return newArray
+  }
+  
 
-export {
-  forEach,
-  map,
-  filter,
-  reduce,
-}
+Array.prototype.myForeach = function(callback){
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this)
+    }
+  }
+
+Array.prototype.myReduce = function(callback, initialValue=0){
+    for (let i = 0; i < this.length; i++) {
+      initialValue = callback(initialValue, this[i], i, this)
+    }
+    return initialValue
+  }
+
+Array.prototype.myFilter = function(callback){
+        let filtered=[]
+        for(let i=0;i< this.length;i++)
+        {
+           if(callback(this[i]))
+             filtered.push(this[i]) 
+        }
+    
+        return filtered
+    }
+
